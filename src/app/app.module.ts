2ce56d9e8a98a7eedfd5f0fed010d0710160be37
thirumani.raja <thirumani.raja@exportify.in>
2020-02-28 17:21:05 +0530

    login initial setup _thiru_ex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +35,8 @@ import {MatButtonModule} from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [],
